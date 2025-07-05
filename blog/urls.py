556358312f8d72a2496f983c84e6d23a860bea20
Reazordd <126767396+Reazordd@users.noreply.github.com>
2025-07-05 21:13:58 +0300

    Создать маршрут для вывода формы авторизации пользователя. (только вывод - GET) Попробуйте самостоятельно реализовать функционал обновления поста. (Необязательно!)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
-from .views import get_post_list, get_post_detail, create_post
+from .views import get_post_list, get_post_detail, create_post, login_view, \
+    update_post  # Импортируем новое представление
 
 urlpatterns = [
     path("posts/", get_post_list, name="post_list"),
     path('posts/<int:post_id>/', get_post_detail, name="post_detail"),
-    path('posts/add/', create_post, name="new_post")
+    path('posts/add/', create_post, name="new_post"),
+    path('login/', login_view, name="login"),
+    path('posts/<int:post_id>/edit/', update_post, name="update_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -25,3 +25,19 @@ def create_post(request):
         post = Post.objects.create(title=request.POST.get('title'), text=request.POST.get('text'))
 
         return redirect('post_detail', post_id=post.id)
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'blog/login.html')
+
+def update_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == "GET":
+        return render(request, 'blog/post_edit.html', {'post': post})
+
+    if request.method == "POST":
+        post.title = request.POST.get('title', post.title)  # Обновляем, если передано
+        post.text = request.POST.get('text', post.text)
+        post.save()
+        return redirect('post_detail', post_id=post.id)
